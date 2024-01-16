0eb3d08c6807db8a938e7c32a8653b7cf93c0d94
ryooosk <ryooosk2143@gmail.com>
2024-01-17 01:33:20 +0900

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import tamaki from "./images/tamaki.jpg"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const wrapper = {
+  width: "800px",
+  display: "flex",
+  border: "3px dashed black",
+  padding: "20px",
+  margin: "20px auto",
+  background: "#FFCCCC",
+}
+
+const intro = {
+  width: "400px",
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div style={wrapper}>
+    <div style={intro} >
+      <h1>玉置優里</h1>
+      <h2>生年月日</h2>
+      <h3>2001年1月19日</h3>
+      <h2>出身地</h2>
+      <h3>大阪府東大阪市稲田本町</h3>
+      <h2>出身高校</h2>
+      <h3>大阪府立市岡高等学校</h3>
+      <h2>趣味</h2>
+      <h3>ネコと戯れること</h3>
+    </div>
+    <div>
+      <img src="./images/tamaki.jpg" alt="tamaki" width="250" />
+    </div>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
